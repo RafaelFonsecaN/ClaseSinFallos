5d6f98f66c26f49f1de0494539b10c980d7dfd39
RafaelFonsecaN <rafa_fonck@hotmail.com>
2025-05-08 18:05:16 -0500

    Se agrego un tercer sumando

--- Sumar.py
+++ Sumar.py
@@ -7,8 +7,9 @@ def calcular_raiz():
   try:
     numero1 = float(request.args.get('numero1'))
     numero2 = float(request.args.get('numero2'))
+    numero3 = float(request.args.get('numero3'))
     
-    resultado = numero1 + numero2
+    resultado = numero1 + numero2 + numero3
     return jsonify({'La suma es igual a': resultado})
   except (TypeError, ValueError):
     return jsonify({'error':'Parametro invalido, se requiere un numero'}),400
